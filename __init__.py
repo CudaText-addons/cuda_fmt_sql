be3c32a1bf96e02e2ee76cdc35275fbb90087143
Alexey-t <support@uvviewsoft.com>
2019-07-11 23:43:00 +0300

    json config

--- __init__.py
+++ __init__.py
@@ -1,15 +1,22 @@
 import sys
 import os
+import re
+import json
 from cuda_fmt import get_config_filename
 
 sys.path.append(os.path.dirname(__file__))
 from . import sqlparse
 
 def opt():
-    ini = get_config_filename('SQL Format')
-    with open(ini) as f:
-        text = f.read()
-    return eval(text)
+    fn = get_config_filename('SQL Parse')
+    if os.path.isfile(fn):
+        s = open(fn, 'r').read()
+        #del // comments
+        s = re.sub(r'(^|[^:])//.*', r'\1', s)
+        d = json.loads(s)
+    else:
+        d = {}
+    return d
 
 def do_format(text):
     return sqlparse.format(text, **opt() )
