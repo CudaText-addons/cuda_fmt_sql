385e2b9c98fd1818a0f4c044b28939904bc7b628
Alexey-t <support@uvviewsoft.com>
2019-07-11 20:41:01 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from cuda_fmt import get_config_filename
+
+sys.path.append(os.path.dirname(__file__))
+from . import sqlparse3 as sqlparse
+
+def opt():
+    ini = get_config_filename('SQL Format')
+    with open(ini) as f:
+        text = f.read()
+    return eval(text)
+
+def do_format(text):
+    return sqlparse.format(text, **opt() )
---
+++ readme/readme.txt
@@ -0,0 +1,7 @@
+Formatter for SQL text (lexer name can be any started with "SQL").
+Uses sqlparse lib:
+https://code.google.com/p/python-sqlparse/
+
+Author: Alexey Torgashin (CudaText)
+License: MIT
+
